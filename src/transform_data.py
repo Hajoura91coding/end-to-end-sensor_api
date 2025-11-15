39186eac98872104fbb434130b9baec03060ec25
hajer <hajer.blohorn@gmail.com>
2025-11-15 21:59:22 +0100

    feat(transform): merge CSV files and aggregate visits by day

--- src/transform_data.py
+++ src/transform_data.py
@@ -1,14 +1,26 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as F
 import os
 
 spark = SparkSession.builder.appName('LoadCSV').getOrCreate()
 
 data_path = os.path.expanduser('~/end-to-end-sensor-api/data/raw/')
 files_list = [f for f in os.listdir(data_path) if f.endswith('.csv')]
-dataframes = {}
 
-for i, file in enumerate(files_list):
+all_data = None
+for file in files_list:
     file_path = os.path.join(data_path, file)
-    dataframes[i] = spark.read.csv(file_path, header=True, inferSchema=True)
-    print(f"Dataframe {i}: {file}")
-    dataframes[i].show()
+    df = spark.read.csv(file_path, header=True, inferSchema=True)
+    if all_data is None:
+        all_data = df
+    else:
+        all_data = all_data.union(df)
+all_data.show(5)
+
+
+data_by_day = (all_data
+                  .groupBy('date','id_magasin')
+                  .agg(F.round(F.sum('nombre_visiteurs'),2).alias('total_visiteurs'))
+                  .orderBy('date'))
+
+data_by_day.show()
